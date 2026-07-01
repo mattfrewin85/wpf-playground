05893c8efe72344dce93b86502825b569c9b811a
mattfrewin85 <nitromundi@googlemail.com>
2026-07-01 12:41:54 +0100

    Add project files.

---
+++ WpfPlayground/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPlayground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(BrightnessValue, BrightnessSlider);
+            //BrightnessValue.Text = BrightnessSlider.Value.ToString();
+        }
+
+        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(ContrastValue, ContrastSlider);
+            //ContrastValue.Text = ContrastSlider.Value.ToString();
+        }
+
+        private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(SaturationValue, SaturationSlider);
+            //SaturationValue.Text = SaturationSlider.Value.ToString();
+        }
+
+        private void UpdateSliderValueTextBlock(TextBlock target, Slider source)
+        {
+            target.Text = source.Value.ToString();
+        }
+    }
+}
