5e3b3a1cb52d08c03835f4f9c5f513da2323087a
mattfrewin85 <nitromundi@googlemail.com>
2026-07-01 15:14:12 +0100

    Added a main menu selection screen, to allow navigation of different test screens

--- WpfPlayground/MainWindow.xaml.cs
+++ WpfPlayground/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WpfPlayground
 {
@@ -21,24 +12,10 @@ namespace WpfPlayground
             InitializeComponent();
         }
 
-        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            UpdateSliderValueTextBlock(BrightnessValue, BrightnessSlider);
-        }
-
-        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            UpdateSliderValueTextBlock(ContrastValue, ContrastSlider);
-        }
-
-        private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            UpdateSliderValueTextBlock(SaturationValue, SaturationSlider);
-        }
-
-        private void UpdateSliderValueTextBlock(TextBlock target, Slider source)
-        {
-            target.Text = source.Value.ToString();
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.ShowDialog();
         }
     }
 }
---
+++ WpfPlayground/SettingsWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfPlayground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(BrightnessValue, BrightnessSlider);
+        }
+
+        private void ContrastSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(ContrastValue, ContrastSlider);
+        }
+
+        private void SaturationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            UpdateSliderValueTextBlock(SaturationValue, SaturationSlider);
+        }
+
+        private void UpdateSliderValueTextBlock(TextBlock target, Slider source)
+        {
+            target.Text = source.Value.ToString();
+        }
+    }
+}
